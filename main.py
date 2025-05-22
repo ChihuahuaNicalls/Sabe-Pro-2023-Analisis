3d17aac4324037b254747ba1d8650fe51251dac7
Chihuahua_Nicalls <ch.sebastian16@hotmail.com>
2025-05-22 16:57:45 -0500

    filter for more data

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ cods = [1301, 1101, 1102, 1103, 1104, 1206, 1701, 1702, 1831, 1731]
 grupo = "INGENIERÍA"
 
 dataFiltered = data[
-    data['inst_cod_institucion'].isin(cods) &
-    (data['gruporeferencia'] == grupo)
+    data['gruporeferencia'] == grupo
 ]
 
 dataFiltered.to_excel('icfesFiltered.xlsx', index=False)
